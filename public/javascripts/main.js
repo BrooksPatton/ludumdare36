504cbba594e47465cc51ca46d395f153574ef685
Brooks Patton <brooks@brookzerker.com>
2016-08-26 22:35:45 -0600

    random stars being created now

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,24 @@
 $().ready(() => {
-  let star = new Star();
+  createStars(300);
 
-  star.create();
-  star.render({top: 15,left: 25});
+  function createStars(numOfStars) {
+    for (var i = 0; i < numOfStars; i++) {
+      let star = new Star();
+      star.create();
+      star.render(randomStarPosition());
+    }
+  }
+
+  function randomStarPosition() {
+    return {
+      left: randomInt(10, $('.star-map').width() - 10),
+      top: randomInt(10, $('.star-map').height() - 10)
+    };
+  }
+
+  function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 });
--- public/javascripts/star-class.js
+++ public/javascripts/star-class.js
@@ -9,11 +9,33 @@ class Star {
   }
 
   render(position) {
+    this.position = position;
+
     this.$star.css({
       left: position.left,
       top: position.top
     });
 
-    $('.star-map').append(this.$star);
+    if(!this._isColliding()) $('.star-map').append(this.$star);
+  }
+
+  _isColliding() {
+    let $stars = $('.star');
+    let collision = false;
+
+    $stars.each((i, star) => {
+      let position = $(star).position();
+
+      if (
+        this.position.left < position.left + 20 &&
+        this.position.left + 20 > position.left &&
+        this.position.top < position.top + 20 &&
+        20 + this.position.top > position.top
+      ) {
+        collision = true;
+      }
+    });
+
+    return collision;
   }
 }
